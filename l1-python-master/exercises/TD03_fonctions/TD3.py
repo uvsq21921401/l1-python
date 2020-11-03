66ceb8090dd9a277f49ac67a98be6586f33053f4
Pauline STAELENS <pauline.staelens@ens.uvsq.fr>
2020-11-03 18:54:10 +0100

    terminer

--- l1-python-master/exercises/TD03_fonctions/TD3.py
+++ l1-python-master/exercises/TD03_fonctions/TD3.py
@@ -42,6 +42,7 @@ def afficheTemps(temps):
     afficheUnitéTemps(temps[1], "Heure")
     afficheUnitéTemps(temps[2], "Minute")
     afficheUnitéTemps(temps[3], "Seconde")
+    print("\n")
     return
     
 #afficheTemps((1,0,14,23))    
@@ -83,7 +84,7 @@ def proportionTemps(temps,proportion):
 
 """ Dite si une année est bissextile pour retire le bon nb de jour dans une année """
 def bissextile_1(année, jour):
-    if année % 4 == 0 and not (année % 100 != 0 and not (année % 400 == 0)):
+    if année % 4 == 0 and not (année % 100 == 0 and not (année % 400 == 0)):
         jour -= 366 #bissextile
     else :
         jour -= 365 #pas bissextile
@@ -97,7 +98,8 @@ def tempsEnDate(temps):
         date[0] += 1
     return date
 
-def afficheDate(date = -1):
+import time
+def afficheDate(date = tempsEnDate(secondeEnTemps(time.time()))):
     if (type(date) == (type(0))):
         return
     afficheUnitéTemps(date[0] + 1970, "Année")
@@ -105,6 +107,7 @@ def afficheDate(date = -1):
     afficheUnitéTemps(date[2], "Heure")
     afficheUnitéTemps(date[3], "Minute")
     afficheUnitéTemps(date[4], "Seconde")
+    print("\n")
     return
 
 #temps = secondeEnTemps(1000000000)
@@ -112,5 +115,60 @@ def afficheDate(date = -1):
 #afficheDate(tempsEnDate(temps))
 #afficheDate()
 
-import time
-afficheDate(time.time())
+#import time
+#afficheDate(tempsEnDate(secondeEnTemps(time.time())))
+#afficheDate(time.gmtime(secondeEnTemps(time.time())))
+#Elle ne s'exécute pas car time.gmtime return une struct et pas une liste !!
+
+
+def bisextile(jour):
+    année = 2020
+    while jour > 365 :
+        if année % 4 == 0 and not (année % 100 == 0 and not (année % 400 == 0)):
+            jour -= 366
+            print(année)
+        else :
+            jour -=365
+        année += 1
+    return
+        
+#bisextile(20000)
+
+def nombreBisextile(jour):
+    année = 1970
+    nb_B = 0
+    while jour > 365 :
+        if année % 4 == 0 and not (année % 100 == 0 and not (année % 400 == 0)):
+            jour -= 366
+            nb_B += 1 
+        else :
+            jour -=365
+        année += 1
+    return nb_B
+
+def tempsEnDateBisextile(temps):
+    temps = [0, temps[0], temps[1], temps[2], temps[3]]
+    nb_B = nombreBisextile(temps[1])
+    while temps[1] > 365 :
+        temps[1] -= 365
+        temps[0] += 1
+    temps[1] -= nb_B
+    return temps
+    
+#temps = secondeEnTemps(1000000000)
+#afficheTemps(temps)
+#afficheDate(tempsEnDateBisextile(temps))
+
+def verifie(liste_temps):
+    secondeTotal = 0
+    for i in liste_temps :
+        s = tempsEnSeconde(i)
+        if s > 3600 * 48 :
+            return False
+        secondeTotal += s
+    if secondeTotal > 3600 * 140 :
+        return False
+    return True
+
+liste_temps = [[1,2,39,34],[0,1,9,4],[0,29,39,51],[0,31,13,46]]
+print(verifie(liste_temps))
