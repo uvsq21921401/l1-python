87bc95cb2a85bc279d782d11624d0a3efa048ee4
Pauline STAELENS <pauline.staelens@ens.uvsq.fr>
2020-11-18 18:48:13 +0100

    En cour

--- l1-python-master/exercises/TD04_listes/Syracuse.py
+++ l1-python-master/exercises/TD04_listes/Syracuse.py
@@ -14,10 +14,44 @@ def syracuse(n) :
 print(syracuse(3))
 
 
-def testeConjecture(n_max) :
-    syracuse(n_max)
-    if n_max == 1 :
+def testeConjecture(n_max):
+    """ Teste la conjecture de Collatz pour toutes les valeurs de 2 à n_max """
+    n_min = 1
+    while n_min < n_max :
+        n_min += 1
+        syracuse(n_min)
+    if n_min == 1 :
         return True
-
+    return False
 
 print(testeConjecture(10000))
+
+
+
+
+def tempsVol(n):
+    """ Retourne le temps de vol de n """
+    liste = []
+    while n != 1 :
+        if n % 2 == 0 :
+            n //= 2
+            liste.append(n)
+        elif n % 2 != 0 :
+            n *= 3
+            n += 1
+            liste.append(n)
+    return len(liste)
+
+
+
+
+def tempsVolListe(n_max):
+    """ Retourne la liste de tous les temps de vol de 1 à n_max """
+    n_min = 0
+    L_max =[]
+    while n_min + 1 > n_max :
+        tempsVol(n_min)
+        L_max.append(n_min)
+    return L_max
+
+print(tempsVolListe(100))
