f45a9338b4c7ecfe004ae826eb92c1cff7dd201f
Pauline STAELENS <pauline.staelens@ens.uvsq.fr>
2020-11-18 17:04:13 +0100

    en cour

--- l1-python-master/exercises/TD04_listes/syracuse.py
+++ l1-python-master/exercises/TD04_listes/syracuse.py
@@ -1,14 +1,14 @@
-#Part 2
- #Retourne la liste des valeurs de la suite en partant de n jusqu'à 1
-
-
-def syracuse(n) :
+ def syracuse(n):
+    """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
+    liste =[]
     while n != 1 :
         if n % 2 == 0 :
-            n /= 2
+            n //= 2
+            liste.append(n)
         elif n % 2 != 0 :
             n *= 3
             n += 1
-    return(n)
+            liste.append(n)
+    return(liste)
 
 print(syracuse(3))
