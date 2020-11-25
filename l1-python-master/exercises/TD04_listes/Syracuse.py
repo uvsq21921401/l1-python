33c89693a7f6aaf86db9b5e6178195969dd1317f
Pauline STAELENS <pauline.staelens@ens.uvsq.fr>
2020-11-25 22:51:29 +0100

    Terminer

--- l1-python-master/exercises/TD04_listes/Syracuse.py
+++ l1-python-master/exercises/TD04_listes/Syracuse.py
@@ -45,3 +45,56 @@ def tempsVol(n):
 
 
 
+def estCarreMagique(carre):
+    """ Renvoie True si c'est un carre magique et False sinon"""
+    cote = len(carre)
+    cst_mag = 0
+    for h in range(cote) :
+        cst_mag += carre[0][h] 
+    for i in range(1, cote) :
+        somme = 0
+        for j in range(cote) :
+            somme += carre[i][j]
+        if cst_mag != somme :
+            return False
+    for k in range(cote) :
+        somme = 0
+        for l in range(cote) :
+            somme += carre[l][k]
+        if cst_mag != somme :
+            return False
+    for m in range(cote) :
+        somme = 0
+        somme += carre[m][m]
+    if cst_mag != somme :
+        return False
+    for n in range(cote) :
+        somme = 0
+        somme += carre[n][cote - n - 1]
+    if cst_mag != somme :
+        return False
+    return True
+
+carre_mag = [[4, 14, 15, 1], [9, 7, 6, 12], [5, 11, 10, 8], [16, 2, 3, 13]]
+carre_pas_mag = [[4, 14, 15, 1], [9, 7, 6, 12], [5, 11, 10, 8], [16, 2, 7, 13]]
+print(estCarreMagique(carre_mag))
+print(estCarreMagique(carre_pas_mag))
+
+
+
+
+from math import sqrt
+
+def estNormal(carre):
+    """ Retourne True si contient toutes les valeurs de 1 à n^2 où n est la taille 
+        du carré, et False sinon """
+    cote = len(carre)
+    if str(int(sqrt(cote))) != "  "  :
+        return False
+    else : 
+        return True
+
+carre_mag = [[4, 14, 15, 1], [9, 7, 6, 12], [5, 11, 10, 8], [16, 2, 3, 13]]
+carre_pas_mag = [[4, 14, 15, 1], [9, 7, 6, 12], [5, 11, 10, 8], [16, 2, 7, 13], [16, 2, 7, 13]]
+print(estNormal(carre_mag))
+print(estNormal(carre_pas_mag))
